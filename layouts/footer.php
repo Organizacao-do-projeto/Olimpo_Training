64229acc8e45a8776075f721770b0cfb0aeb7c45
glauber-odev <111056180+glauber-odev@users.noreply.github.com>
2023-10-28 08:01:32 -0300

    footer adcionado

---
+++ layouts/footer.php
@@ -0,0 +1,87 @@
+<style>
+.footerRoot {
+  height: 377px;
+  background: linear-gradient(0deg, #000000 0%, gold 289.06%);
+  margin: 0 auto;
+  width: 100%;
+}
+
+.flex{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.flex-column{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.container{
+    width: 80vw;
+    margin: 0 auto;
+    height: 100%;
+}
+
+
+.footerRoot section img {
+  height: 45px;
+  width: 45px;
+}
+
+.footerRoot figure img {
+  height: 200px;
+  width: 200px;
+}
+
+.container > section > h2 {
+  height: 47px;
+  width: 668px;
+  left: 5446px;
+  font-family: 'Rubik';
+  font-size: 48px;
+  font-weight: 400;
+  line-height: 47px;
+  text-align: center;
+  color: #ffffff;
+}
+
+.container > section > h3 {
+  height: 31px;
+  width: 449px;
+  font-family: 'Rubik';
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 31px;
+  text-align: left;
+  color: #ffffff;
+  margin: 0 auto;
+  margin-top: 15px;
+}
+
+.footer-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+}
+
+</style>
+<footer class="footerRoot">
+    <a href="../index.php">
+      <div class="container flex">
+        <figure>
+          <img src="../assets/img/logos/logo_branca_alternative.png" />
+        </figure>
+    </a>
+        <section class="flex-column">
+          <h2>TRANSFORME O SEU TREINO AQUI.</h2>
+          <h3>SIGA NOSSAS REDES SOCIAIS</h3>
+          <div class="footer-icons">
+            <a href=""><img src="https://cdn-icons-png.flaticon.com/512/3955/3955024.png" /></a>
+            <a href=""><img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" /></a>
+          </div>
+        </section>
+      </div>
+</footer>
