5435593c180a42f1e0117ab128f20bb0f0591d57
lucasykw <144175948+lucasykw@users.noreply.github.com>
2023-11-01 11:03:36 -0300

    Atualização no texto

--- usuarioPersonal/index.php
+++ usuarioPersonal/index.php
@@ -15,7 +15,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste CRUD</title>
+    <title>CRUD - Personal</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
